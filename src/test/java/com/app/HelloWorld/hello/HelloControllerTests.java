f785953b9a87281bd30a0a6a2fb01686f8e250c6
Elena Vega-Fujioka <elenavegafujioka@yahoo.com>
2021-06-03 18:27:40 -0700

    junit fixes

--- src/test/java/com/app/HelloWorld/hello/HelloControllerTests.java
+++ src/test/java/com/app/HelloWorld/hello/HelloControllerTests.java
@@ -1,12 +1,7 @@
+package com.app.HelloWorld.hello;
 
-import org.junit.After;
 import org.junit.Assert.assertEquals;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 /** * Created by  * Sample junit test code to integrate by */
 public class HelloControllerTests{    
