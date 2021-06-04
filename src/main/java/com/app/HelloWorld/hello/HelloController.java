82e82ff4f8a8406e0c5160e6494bdc0496bf91f8
Elena Vega-Fujioka <elenavegafujioka@yahoo.com>
2021-06-03 18:19:00 -0700

    add j unit test

--- src/main/java/com/app/HelloWorld/hello/HelloController.java
+++ src/main/java/com/app/HelloWorld/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController{
     @RequestMapping("/hello")
-    public String sayHello(){
+    public static String sayHello(){
         return "Hello World! ";
     }
 }
---
+++ src/test/java/com/app/HelloWorld/hello/HelloControllerTests.java
@@ -0,0 +1,21 @@
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/** * Created by  * Sample junit test code to integrate by */
+public class HelloControllerTests{    
+	String message = "Hello World!";	
+   
+	@Test
+	public void testPrintMessage() {
+	   Assert.assertEquals(HelloController.sayHello(), "Hello World! ");
+	}
+
+
+}
