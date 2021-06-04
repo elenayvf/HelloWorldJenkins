f40b87758018d418803d48183faa4aa75f8693a1
Elena Vega-Fujioka <elenavegafujioka@yahoo.com>
2021-06-03 17:35:48 -0700

    fix web app

--- src/main/java/com/app/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/app/HelloWorld/HelloWorldApplication.java
@@ -1,9 +1,10 @@
 package com.app.HelloWorld;
-
+import com.app.HelloWorld.hello.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan(basePackageClasses = HelloController.class)
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
