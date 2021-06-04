e6363fc5655dbecb31d963ee3c675a412672a591
Elena Vega-Fujioka <elenavegafujioka@yahoo.com>
2021-06-03 20:33:46 -0700

    make war file

--- src/main/java/com/app/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/app/HelloWorld/HelloWorldApplication.java
@@ -3,9 +3,11 @@ import com.app.HelloWorld.hello.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 @ComponentScan(basePackageClasses = HelloController.class)
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
